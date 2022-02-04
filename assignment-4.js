526ddf034e727c04a37a98e0a05c69d6f1b9c7c1
iamyeasirarafat <yas.yeasir@gmail.com>
2022-02-05 03:10:16 +0600

    3rd problem done

--- assignment-4.js
+++ assignment-4.js
@@ -63,3 +63,40 @@ function pandaCost(shingara, somucha, jilapi) {
 }
 
 
+/* ৩. [ ফাংশন নেম দিতে হবে picnicBudget]: প্যারামিটার হিসাবে নিবে কয়জন পিকিনিক এ যাবে সেই সংখ্যা। 
+
+যদি ১০০ বা তার কম যায় তাহলে প্রত্যেক এর জন্য খরচ হবে ৫০০০ টাকা করে। 
+
+যদি ১০০ এর বেশি কিন্তু ২০০ এর কম বা সমান যায় তাহলে প্রথম ১০০ জন এর প্রতি জন এর জন্য ৫০০০ টাকা করে দিতে হবে। আর ১০০ এর বেশি (অর্থাৎ ১০১ নম্বর থেকে) যতজন আছে তাদের প্রত্যেক ওই জন্য ৪০০০ টাকা করে দিতে হবে। 
+
+আর যদি ২০০ এর বেশি যায় তাহলে প্রথম ১০০ জন এর প্রতি জন এর জন্য ৫০০০ টাকা করে দিতে হবে। আর ১০০ এর বেশি যতজন আছে তাদের প্রত্যেকের জন্য ৪০০০ টাকা করে দিতে হবে। এর ২০০ এর বেশি (অর্থাৎ ২০১ নম্বর থেকে) যতজন আছে তাদের প্রত্যেক এর জন্য ৩০০০ টাকা করে দিতে হবে।  
+
+
+
+এখন আমি একটা প্যারামিটার দিবো। সেটা যেকোন সংখ্যা হতে পারে। সেই সংখ্যা অনুসারে তুমি ফাংশন থেকে রিটার্ন দিবে কতটাকা পিকনিক এর বাজেট হতে হবে। ইনপুট প্যারামিটার সংখ্যাটা ১০০ এর কম হতে পারে। ১০০ থেকে ২০০ এর মধ্যে হতে পারে। আবার ২০০ এর বেশিও হতে পারে। তাই সব কেইস ভালো করে টেস্ট করে নিবে।  */
+
+function picnicBudget(numberOfPeoples) {
+  if (typeof numberOfPeoples != 'number') {
+    const error = 'please input number value only' 
+    return error;       
+}else if(numberOfPeoples<0){
+const error = 'please give positive value only';
+return error;        
+}else{
+  if (numberOfPeoples<=100) {
+    let totalBudget = numberOfPeoples * 5000;
+    return totalBudget;
+  }else if (numberOfPeoples<=200) {
+    let extraPeople = numberOfPeoples - 100;
+    let extraPeopleBudget = extraPeople *4000;
+    let totalBudget = 500000 + extraPeopleBudget;
+    return totalBudget;
+  }else if (numberOfPeoples > 200) {
+    let extraPeople = numberOfPeoples - 200;
+    let extraPeopleBudget = extraPeople * 3000;
+    let totalBudget = 500000 + 400000 + extraPeopleBudget;
+    return totalBudget;
+  }
+}
+}
+
